06e5479422b901b2408da033f739596b96acd44a
pedroandr3s <pedroverareal@gmail.com>
2025-08-29 12:30:03 -0400

    ID usuario

--- Nodos/NodoSensorColmena/NodoSensor/Config.h
+++ Nodos/NodoSensorColmena/NodoSensor/Config.h
@@ -7,18 +7,18 @@
 
 // --- INICIO CONFIGURACION ---
 #define SMARTBEE_DEBUG 1                     // Modo depuración.  0 = debug desactivado, 1 = debug activado
-#define SMARTBEE_MQTT_SERVER "broker.emqx.io"   // direccion IP del servidor MQTT (o websocket)
-#define SMARTBEE_MQTT_PORT SMARTBEE_MQTT_NML // seleccionar SMARTBEE_MQTT_NML, SMARTBEE_MQTT_TLS o SMARTBEE_MQTT_WSS
+#define SMARTBEE_MQTT_SERVER "smartbee.cl"   // direccion IP del servidor MQTT (o websocket)
+#define SMARTBEE_MQTT_PORT SMARTBEE_MQTT_TLS // seleccionar SMARTBEE_MQTT_NML, SMARTBEE_MQTT_TLS o SMARTBEE_MQTT_WSS
 #define SMARTBEE_WIFI_SSID "Colmena"         // Nombre de la wifi
 #define SMARTBEE_WIFI_PASS "@Abejita123"      // Clave de la wifi
 #define SMARTBEE_MQTT_NODE "NODO-BEF8C985-0FF3-4874-935B-40AA8A235FF7"         // ID del nodo autorizado a publicar
-#define SMARTBEE_MQTT_PASS "Clave nodo"      // Clavel del nodo autorizado para publicar
+#define SMARTBEE_MQTT_PASS "pha1lozee6thah6ai6ej"      // Clavel del nodo autorizado para publicar
 #define SMARTBEE_LOADCELL_ENABLED 1          // establece si se incluye o no el modulo de peso. 0 = no, 1= si
 #define SMARTBEE_LOADCELL_CALIBRATION 2280.f // resultado de la calibración
 // --- FIN CONFIGURACION ---
 
 #define SMARTBEE_MQTT_CLIENTID SMARTBEE_MQTT_NODE                        // id unico de este nodo
-#define SMARTBEE_MQTT_TOPIC "testtopic/smartbee" // topico en donde publicar
+#define SMARTBEE_MQTT_TOPIC "SmartBee/nodes/" SMARTBEE_MQTT_NODE "/data" // topico en donde publicar
 #define SMARTBEE_MQTT_WSS_PATH "/apps/mqtt.rcr"                          // ruta para acceder a MQTT via websocket
 #define SMARTBEE_SLEEP_US_NORMAL (20 * 60 * 1000000)                     // deep sleep normal cada 20 minutos
 #define SMARTBEE_SLEEP_US_ERROR (5 * 60 * 1000000)                       // deep sleep ante error cada 5 minutos
--- Nodos/NodoSensorExterior/NodoSensor/Config.h
+++ Nodos/NodoSensorExterior/NodoSensor/Config.h
@@ -6,19 +6,18 @@
 #define SMARTBEE_MQTT_NML 1883 // Usar contra servidor local: puerta para MQTT local
 
 // --- INICIO CONFIGURACION ---
-#define SMARTBEE_DEBUG 1                     // Modo depuración.  0 = debug desactivado, 1 = debug activado
-#define SMARTBEE_MQTT_SERVER "broker.emqx.io"   // direccion IP del servidor MQTT (o websocket)
-#define SMARTBEE_MQTT_PORT SMARTBEE_MQTT_NML // seleccionar SMARTBEE_MQTT_NML, SMARTBEE_MQTT_TLS o SMARTBEE_MQTT_WSS
+#define SMARTBEE_MQTT_SERVER "smartbee.cl"   // direccion IP del servidor MQTT (o websocket)
+#define SMARTBEE_MQTT_PORT SMARTBEE_MQTT_TLS // seleccionar SMARTBEE_MQTT_NML, SMARTBEE_MQTT_TLS o SMARTBEE_MQTT_WSS
 #define SMARTBEE_WIFI_SSID "Colmena"         // Nombre de la wifi
 #define SMARTBEE_WIFI_PASS "@Abejita123"      // Clave de la wifi
 #define SMARTBEE_MQTT_NODE "NODO-B5B3ABC4-E0CE-4662-ACB3-7A631C12394A"         // ID del nodo autorizado a publicar
-#define SMARTBEE_MQTT_PASS "clave nodo"      // Clavel del nodo autorizado para publicar
+#define SMARTBEE_MQTT_PASS "eeheeut4Ph0oocer65"      // Clavel del nodo autorizado para publicar
 #define SMARTBEE_LOADCELL_ENABLED 0          // establece si se incluye o no el modulo de peso. 0 = no, 1= si
-#define SMARTBEE_LOADCELL_CALIBRATION 23.25 // resultado de la calibración
+#define SMARTBEE_LOADCELL_CALIBRATION 2280.f // resultado de la calibración
 // --- FIN CONFIGURACION ---
 
 #define SMARTBEE_MQTT_CLIENTID SMARTBEE_MQTT_NODE                        // id unico de este nodo
-#define SMARTBEE_MQTT_TOPIC "testtopic/smartbee" // topico en donde publicar
+#define SMARTBEE_MQTT_TOPIC "SmartBee/nodes/" SMARTBEE_MQTT_NODE "/data" // topico en donde publicar
 #define SMARTBEE_MQTT_WSS_PATH "/apps/mqtt.rcr"                          // ruta para acceder a MQTT via websocket
 #define SMARTBEE_SLEEP_US_NORMAL (20 * 60 * 1000000)                     // deep sleep normal cada 20 minutos
 #define SMARTBEE_SLEEP_US_ERROR (5 * 60 * 1000000)                       // deep sleep ante error cada 5 minutos
